1a24f0cdb817e4e56cdb274d5cdfd36e22dd6dce
johnnyyang171 <116053159+johnnyyang171@users.noreply.github.com>
2022-12-31 17:39:13 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ let number = 0
 input.onGesture(Gesture.Shake, function () {
     basic.clearScreen()
     number = randint(1, 3)
-    if (number == 3) {
+    if ((0 as any) == (3 as any)) {
         basic.showIcon(IconNames.Yes)
     } else if (number == 2) {
         basic.showIcon(IconNames.No)
